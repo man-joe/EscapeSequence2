29ef0d8685a467d99527cfd6646e947c964ebbd0
man-joe <jhoang3@gmail.com>
2019-09-09 10:35:17 -0400

    Added README and Escape Sequence 2

---
+++ src/EscapeSequence2.java
@@ -0,0 +1,9 @@
+public class EscapeSequence2 {
+    public static void main(String[] args) {
+        System.out.println("Cost:\tQuantity\tTotal");
+        System.out.println("$1,000.00\t4\t$4,000.00");
+        System.out.println("   $50.00\t8\t$400.00");
+        System.out.println("_________________________________");
+        System.out.println("TOTAL:\t\t12\t$4,400.00");
+    }
+}
